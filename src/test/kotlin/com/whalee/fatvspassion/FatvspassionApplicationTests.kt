ca67aa52d0bab5ecbc1abaf8fffe61941a25a686
Seungmin123 <dltmdals147@gmail.com>
2023-06-14 12:00:54 +0900

    Start

---
+++ src/test/kotlin/com/whalee/fatvspassion/FatvspassionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.whalee.fatvspassion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FatvspassionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
